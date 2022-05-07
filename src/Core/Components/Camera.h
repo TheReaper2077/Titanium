70348ec6ad345d5866a739509d8924a4c60315d6
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-05-07 20:50:18 +0530

    some stuff

--- src/Core/Components/Camera.h
+++ src/Core/Components/Camera.h
@@ -18,6 +18,7 @@ namespace ti {
 
 			glm::vec3 Up;
 			glm::vec3 Right;
+			glm::vec3 Position = glm::vec3(0, 0, 0);
 			glm::vec3 Front = glm::vec3(0.0f, 0.0f, -1.0f);
 			glm::vec3 WorldUp = glm::vec3(0.0f, 1.0f, 0.0f);
 			glm::vec3 WorldFront = glm::vec3(0.0f, 0.0f, -1.0f);
--- src/Core/System/CameraSystem.cpp
+++ src/Core/System/CameraSystem.cpp
@@ -10,8 +10,6 @@ void ti::System::CameraSystem::Update(double dt) {
 	auto& engine = registry->Store<EngineProperties>();
 	auto& events = registry->Store<Events>();
 
-	std::cout << events.key_toggled.contains(SDL_SCANCODE_F4) << '\n';
-
 	for (auto& entity : registry->View<Transform, Camera>()) {
 		if (registry->Contains<Tag>(entity)) continue;
 
@@ -20,59 +18,73 @@ void ti::System::CameraSystem::Update(double dt) {
 		
 		if (!camera.enable) continue;
 
-		if (camera.mode == PERSPECTIVE) {
-			if (camera.width != engine.width || camera.height != engine.height) {
-				camera.width = engine.width;
-				camera.height = engine.height;
+		float distance;
 
-				camera.lastX = camera.width / 2;
-				camera.lastY = camera.height / 2;
+		if (events.key_toggled.contains(SDL_SCANCODE_F4) && camera.type == Editor)
+			camera.mode = ORTHOGRAPHIC;
+		else
+			camera.mode = PERSPECTIVE;
 
-				camera.first_mouse = true;
+		if (camera.width != engine.width || camera.height != engine.height) {
+			camera.width = engine.width;
+			camera.height = engine.height;
 
+			camera.lastX = camera.width / 2;
+			camera.lastY = camera.height / 2;
+
+			camera.first_mouse = true;
+
+			if (camera.mode == PERSPECTIVE)
 				camera.projection = glm::perspective(glm::radians(camera.fov), (float)camera.width / camera.height, camera.camera_near, camera.camera_far);
-				if (!camera.init) transform.position = glm::vec3(0, 0, 10);
-				camera.init = true;
+			if (camera.mode == ORTHOGRAPHIC) {
+				int widen = std::tan(glm::radians(camera.fov/2.0)) * camera.camera_far;
+				int highten = std::tan(glm::radians((float)(camera.fov / (float)(camera.width/camera.height))/2.0)) * camera.camera_far;
+
+				camera.projection = glm::ortho<float>(-widen, widen, -highten, highten, 0.0, (float)(camera.width/camera.height)*camera.camera_far);
 			}
+			if (!camera.init) camera.Position = glm::vec3(0, 0, 10);
+			camera.init = true;
+		}
 
-			if (events.mouse_pressed.contains(SDL_BUTTON_MIDDLE)) {
-				if (camera.first_mouse) {
-					camera.lastX = events.posx;
-					camera.lastY = events.posy;
-					camera.first_mouse = false;
-				}
-
-				transform.rotation.y -= (events.posx - camera.lastX) * camera.sensitivity;
-				transform.rotation.x += (camera.lastY - events.posy) * camera.sensitivity;
-
+		if (events.mouse_pressed.contains(SDL_BUTTON_MIDDLE)) {
+			if (camera.first_mouse) {
 				camera.lastX = events.posx;
 				camera.lastY = events.posy;
-			} else {
-				camera.first_mouse = true;
+				camera.first_mouse = false;
 			}
 
-			const auto& quaternion = transform.GetRotationQuat();
+			transform.rotation.y -= (events.posx - camera.lastX) * camera.sensitivity;
+			transform.rotation.x += (camera.lastY - events.posy) * camera.sensitivity;
 
-			camera.Front = glm::normalize(quaternion * camera.WorldFront);
-			camera.Up = glm::normalize(quaternion * camera.WorldUp);
+			camera.lastX = events.posx;
+			camera.lastY = events.posy;
+		} else {
+			camera.first_mouse = true;
+		}
 
-			float distance;
+		const auto& quaternion = transform.GetRotationQuat();
 
-			if (events.mouse_scrollup) {
-				transform.position += camera.Front * float(camera.scrollspeed * dt);
-			}
-			if (events.mouse_scrolldown) {
-				transform.position -= camera.Front * float(camera.scrollspeed * dt);
-			}
+		camera.Front = glm::normalize(quaternion * camera.WorldFront);
+		camera.Up = glm::normalize(quaternion * camera.WorldUp);
 
-			distance = glm::length(transform.position - camera.Center);
-			camera.scrollspeed = distance / 10;
-			transform.position = distance * -camera.Front;
 
-			static glm::vec3 delta;
-			static glm::vec3 initpos;
+		if (events.mouse_scrollup) {
+			camera.Position += camera.Front * float(camera.scrollspeed * dt);
+		}
+		if (events.mouse_scrolldown) {
+			camera.Position -= camera.Front * float(camera.scrollspeed * dt);
+		}
 
-			if (events.mouse_pressed.contains(SDL_BUTTON_LEFT) && events.key_pressed.contains(SDL_SCANCODE_LALT)) {
+		distance = glm::length(camera.Position - camera.Center);
+		camera.scrollspeed = distance / 10;
+		camera.Position = distance * -camera.Front;
+
+		static glm::vec3 delta;
+		static glm::vec3 initpos;
+
+		if (events.mouse_pressed.contains(SDL_BUTTON_LEFT) && events.key_pressed.contains(SDL_SCANCODE_LALT)) {
+			glm::vec3 pos;
+			// if (camera.mode == PERSPECTIVE) {
 				glm::vec4 ray_clip = glm::vec4(events.normalized_mouse.x, events.normalized_mouse.y, -1.0, 1.0);
 				glm::vec4 ray_eye = glm::inverse(camera.projection) * ray_clip;
 				ray_eye = glm::vec4(ray_eye.x, ray_eye.y, -1.0, 0.0);
@@ -81,23 +93,35 @@ void ti::System::CameraSystem::Update(double dt) {
 
 				float denom = glm::dot(camera.Front, ray_wor);
 				float t = -glm::dot((camera.Front * distance), camera.Front) / denom;
-				glm::vec3 pos = ray_wor * t + (camera.Offset + transform.position);
+				pos = ray_wor * t + (camera.Offset + camera.Position);
+			// }
+			// if (camera.mode == ORTHOGRAPHIC) {
+			// 	int widen = std::tan(glm::radians(camera.fov/2.0)) * camera.camera_far;
+			// 	int highten = std::tan(glm::radians((float)(camera.fov / (float)(camera.width/camera.height))/2.0)) * camera.camera_far;
 
-				if (!toggle) {
-					initpos = pos;
-				}
+			// 	pos = camera.Offset
+			// }
 
-				delta = pos - initpos;
-				toggle = true;
+			if (!toggle) {
+				initpos = pos;
 			}
 
-			camera.view = glm::lookAt(transform.position + camera.Offset + delta, transform.position + camera.Front + camera.Offset + delta, camera.Up);
+			delta = pos - initpos;
+			toggle = true;
+		}
 
-			if (toggle && !events.mouse_pressed.contains(SDL_BUTTON_LEFT) && events.key_pressed.contains(SDL_SCANCODE_LALT)) {
-				camera.Offset += delta;
-				toggle = false;
-				delta = glm::vec3();
-			}
+		transform.position = camera.Position + camera.Offset + delta;
+
+		if (camera.mode == PERSPECTIVE)
+			camera.view = glm::lookAt(transform.position, transform.position + camera.Front, camera.Up);
+		if (camera.mode == ORTHOGRAPHIC) {
+			// camera.view = glm::translate(glm::mat4(1.0f), transform.position - camera.Offset) * transform.GetRotationMatrix() * glm::scale(glm::mat4(1.0f), glm::vec3(1.0, 1.0, 1.0));
+		}
+
+		if (toggle && !events.mouse_pressed.contains(SDL_BUTTON_LEFT) && events.key_pressed.contains(SDL_SCANCODE_LALT)) {
+			camera.Offset += delta;
+			toggle = false;
+			delta = glm::vec3();
 		}
 	}
 }
